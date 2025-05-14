5b574f5f684e072bdf24c81fc56e149f926d0126
tivvvv <18612553935@163.com>
2025-05-14 12:49:45 +0800

    feat: 线程池配置上报任务.

---
+++ src/main/java/com/tiv/dynamicthreadpool/sdk/trigger/job/ThreadPoolConfigReportJob.java
@@ -0,0 +1,39 @@
+package com.tiv.dynamicthreadpool.sdk.trigger.job;
+
+import com.alibaba.fastjson.JSON;
+import com.tiv.dynamicthreadpool.sdk.domain.DynamicThreadPoolConfigService;
+import com.tiv.dynamicthreadpool.sdk.domain.model.entity.ThreadPoolConfigEntity;
+import com.tiv.dynamicthreadpool.sdk.registry.RegistryService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.List;
+
+/**
+ * 线程池配置上报任务
+ */
+@Slf4j
+@AllArgsConstructor
+public class ThreadPoolConfigReportJob {
+
+    private final DynamicThreadPoolConfigService dynamicThreadPoolConfigService;
+
+    private final RegistryService registryService;
+
+    @Scheduled(cron = "0/30 * * * * ?")
+    public void execute() {
+        log.info("Thread pool config report job starts");
+
+        List<ThreadPoolConfigEntity> threadPoolConfigEntityList = dynamicThreadPoolConfigService.queryThreadPoolConfigList();
+        registryService.reportThreadPoolConfigList(threadPoolConfigEntityList);
+        log.info("report thread pool config list:{}", JSON.toJSONString(threadPoolConfigEntityList));
+
+        for (ThreadPoolConfigEntity threadPoolConfigEntity : threadPoolConfigEntityList) {
+            registryService.reportThreadPoolConfig(threadPoolConfigEntity);
+            log.info("report thread pool config:{}", JSON.toJSONString(threadPoolConfigEntity));
+        }
+
+        log.info("Thread pool config report job ends");
+    }
+}
