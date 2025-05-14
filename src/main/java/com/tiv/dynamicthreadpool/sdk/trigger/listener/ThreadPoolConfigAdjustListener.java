684c6683ccffbba0ab033febf4b72eb189d48557
tivvvv <18612553935@163.com>
2025-05-14 20:07:39 +0800

    feat: 线程池配置调整监听器.

---
+++ src/main/java/com/tiv/dynamicthreadpool/sdk/trigger/listener/ThreadPoolConfigAdjustListener.java
@@ -0,0 +1,35 @@
+package com.tiv.dynamicthreadpool.sdk.trigger.listener;
+
+import com.tiv.dynamicthreadpool.sdk.domain.DynamicThreadPoolConfigService;
+import com.tiv.dynamicthreadpool.sdk.domain.model.entity.ThreadPoolConfigEntity;
+import com.tiv.dynamicthreadpool.sdk.registry.RegistryService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.listener.MessageListener;
+
+import java.util.List;
+
+/**
+ * 线程池配置调整监听器
+ */
+@Slf4j
+@AllArgsConstructor
+public class ThreadPoolConfigAdjustListener implements MessageListener<ThreadPoolConfigEntity> {
+
+    private final DynamicThreadPoolConfigService dynamicThreadPoolConfigService;
+
+    private final RegistryService registryService;
+
+    @Override
+    public void onMessage(CharSequence charSequence, ThreadPoolConfigEntity threadPoolConfigEntity) {
+        log.info("Thread pool config adjust listener detected: {}", threadPoolConfigEntity.toString());
+        dynamicThreadPoolConfigService.updateThreadPoolConfig(threadPoolConfigEntity);
+
+        // 调整配置后重新上报数据
+        List<ThreadPoolConfigEntity> threadPoolConfigEntityList = dynamicThreadPoolConfigService.queryThreadPoolConfigList();
+        registryService.reportThreadPoolConfigList(threadPoolConfigEntityList);
+
+        ThreadPoolConfigEntity threadPoolConfigEntityUpdated = dynamicThreadPoolConfigService.queryThreadPoolConfigByName(threadPoolConfigEntity.getThreadPoolName());
+        registryService.reportThreadPoolConfig(threadPoolConfigEntityUpdated);
+    }
+}
