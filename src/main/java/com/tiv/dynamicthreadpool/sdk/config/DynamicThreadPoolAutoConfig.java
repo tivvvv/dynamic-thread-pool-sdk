294b10912f30d43e5ae5577af4000c739f2a2cc7
tivvvv <18612553935@163.com>
2025-05-16 00:09:16 +0800

    perf: 优化线程池初始化.

--- src/main/java/com/tiv/dynamicthreadpool/sdk/config/DynamicThreadPoolAutoConfig.java
+++ src/main/java/com/tiv/dynamicthreadpool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -1,5 +1,6 @@
 package com.tiv.dynamicthreadpool.sdk.config;
 
+import com.alibaba.fastjson.JSON;
 import com.tiv.dynamicthreadpool.sdk.domain.DynamicThreadPoolConfigService;
 import com.tiv.dynamicthreadpool.sdk.domain.impl.DynamicThreadPoolConfigServiceImpl;
 import com.tiv.dynamicthreadpool.sdk.domain.model.entity.ThreadPoolConfigEntity;
@@ -21,6 +22,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
@@ -35,13 +37,28 @@ public class DynamicThreadPoolAutoConfig {
     private String applicationName;
 
     @Bean("dynamicThreadPoolConfigService")
-    public DynamicThreadPoolConfigService dynamicThreadPoolConfigService(ApplicationContext applicationContext, Map<String, ThreadPoolExecutor> threadPoolExecutorMap) {
+    public DynamicThreadPoolConfigService dynamicThreadPoolConfigService(ApplicationContext applicationContext, Map<String, ThreadPoolExecutor> threadPoolExecutorMap, RedissonClient redissonClient) {
         applicationName = applicationContext.getEnvironment().getProperty("spring.application.name");
 
         if (applicationName == null) {
             applicationName = "unnamed";
             log.warn("Application name not configured in spring.application.name, default value: unnamed");
         }
+
+        // 读取缓存中的线程池配置
+        Set<String> threadPoolNames = threadPoolExecutorMap.keySet();
+        for (String threadPoolName : threadPoolNames) {
+            String cacheKey = String.format("%s_%s_%s", RegistryKeyEnum.THREAD_POOL_CONFIG_KEY.getKey(), applicationName, threadPoolName);
+            ThreadPoolConfigEntity threadPoolConfigEntity = redissonClient.<ThreadPoolConfigEntity>getBucket(cacheKey).get();
+            if (threadPoolConfigEntity == null) {
+                continue;
+            }
+            ThreadPoolExecutor threadPoolExecutor = threadPoolExecutorMap.get(threadPoolName);
+            // 更新线程池配置
+            threadPoolExecutor.setMaximumPoolSize(threadPoolConfigEntity.getMaximumPoolSize());
+            threadPoolExecutor.setCorePoolSize(threadPoolConfigEntity.getCorePoolSize());
+        }
+        log.info("Thread pool configs: {}", JSON.toJSONString(threadPoolExecutorMap));
 
         return new DynamicThreadPoolConfigServiceImpl(applicationName, threadPoolExecutorMap);
     }
